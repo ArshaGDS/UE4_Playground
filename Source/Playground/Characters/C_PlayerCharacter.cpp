ac8b9e0afd1e2abbdb71e96d9e4d237b4a0aee66
Alireza <mrarshax@gmail.com>
2022-10-16 22:43:03 +0330

    Scan Prototype01: Outline material

--- Source/Playground/Characters/C_PlayerCharacter.cpp
+++ Source/Playground/Characters/C_PlayerCharacter.cpp
@@ -3,9 +3,11 @@
 
 #include "C_PlayerCharacter.h"
 
+#include "Ch_Enemy.h"
 #include "I_PlayerInputComponent.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AC_PlayerCharacter::AC_PlayerCharacter()
@@ -22,31 +24,6 @@ AC_PlayerCharacter::AC_PlayerCharacter()
 	GetMesh()->SetOnlyOwnerSee(true);
 	GetMesh()->bCastDynamicShadow = false;
 	GetMesh()->CastShadow = false;
-}
-
-void AC_PlayerCharacter::TabAbility_Implementation()
-{
-	II_PlayerInputInterface::TabAbility_Implementation();
-
-	FVector ViewPointLocation;
-	FRotator ViewPointRotation;
-	GetController()->GetPlayerViewPoint(ViewPointLocation, ViewPointRotation);
-	FVector EndTrace = ViewPointLocation + ViewPointRotation.Vector() * 1000;
-	FHitResult Hit;
-	FCollisionQueryParams Params;
-	Params.AddIgnoredActor(this);
-	GetWorld()->LineTraceSingleByChannel(
-		Hit,
-		ViewPointLocation,
-		EndTrace,
-		ECC_Pawn,
-		Params
-	);
-
-	if (IsValid(Hit.GetActor()))
-	{
-		UE_LOG(LogTemp, Warning, TEXT("[TabAbility]....Hitted...."));
-	}
 }
 
 // Called when the game starts or when spawned
@@ -70,5 +47,50 @@ void AC_PlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputC
 	{
 		II_PlayerInputComponent::Execute_GetPlayerInputComponent(GetWorld()->GetFirstPlayerController(), this,
 			PlayerInputComponent, RotationSpeed);
+	}
+}
+
+void AC_PlayerCharacter::TabAbility_Implementation()
+{
+	II_PlayerInputInterface::TabAbility_Implementation();
+
+	bIsInScanMode = !bIsInScanMode;
+	if (bIsInScanMode)
+	{
+		// Set outline on all enemies
+
+		// Set scan filter on camera
+	}
+	else
+	{
+		// Delete outline on all enemies
+
+		// Delete scan filter on camera
+	}
+	
+	// Get all enemies in the world
+	TArray<AActor*> EnemiesArray;
+	UGameplayStatics::GetAllActorsOfClass(this, ACh_Enemy::StaticClass(), EnemiesArray);
+	// Set outline on enemy mesh
+
+	// Line trace for scan enemy 
+	FVector ViewPointLocation;
+	FRotator ViewPointRotation;
+	GetController()->GetPlayerViewPoint(ViewPointLocation, ViewPointRotation);
+	FVector EndTrace = ViewPointLocation + ViewPointRotation.Vector() * 1000;
+	FHitResult Hit;
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(this);
+	GetWorld()->LineTraceSingleByChannel(
+		Hit,
+		ViewPointLocation,
+		EndTrace,
+		ECC_Pawn,
+		Params
+	);
+
+	if (IsValid(Hit.GetActor()))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[TabAbility]....Hitted...."));
 	}
 }
--- Source/Playground/Characters/C_PlayerCharacter.h
+++ Source/Playground/Characters/C_PlayerCharacter.h
@@ -38,4 +38,8 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+private:
+
+	bool bIsInScanMode{false};
 };
