a0c4f49aa86b8a473ab9469087e9e9db88f01115
Alireza <mrarshax@gmail.com>
2022-11-12 13:57:20 +0330

    SecurityCamera Prototype01: Added security camera pawn with rotation movement

---
+++ Source/Playground/Interactables/P_SecurityCamera.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "P_SecurityCamera.h"
+
+#include "Components/SphereComponent.h"
+#include "GameFramework/RotatingMovementComponent.h"
+
+// Sets default values
+AP_SecurityCamera::AP_SecurityCamera()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraMesh  = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Camera Mesh"));
+	CameraMesh->SetupAttachment(RootComponent);
+	
+	CameraMovement = CreateDefaultSubobject<URotatingMovementComponent>(TEXT("Camera movement component"));
+}
+
+// Called when the game starts or when spawned
+void AP_SecurityCamera::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	MaximumRotationToLeft  = GetActorRotation().Yaw + Degree;
+	MaximumRotationToRight = GetActorRotation().Yaw - Degree;
+	
+	CameraMovement->RotationRate.Yaw = YawDirection;
+	UE_LOG(LogTemp, Display, TEXT("[SecurityCamera]Initial Yaw: %f"), GetActorRotation().Yaw);
+}
+
+// Called every frame
+void AP_SecurityCamera::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// Change the function's name
+	Investigating();
+}
+
+// Called to bind functionality to input
+void AP_SecurityCamera::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AP_SecurityCamera::Investigating()
+{
+	const float CurrentYaw = GetActorRotation().Yaw;
+	if (CurrentYaw >= MaximumRotationToLeft || CurrentYaw <= MaximumRotationToRight)
+	{
+		UE_LOG(LogTemp, Display, TEXT("[SecurityCamera]CurrentYaw: %f"), CurrentYaw);
+		CameraMovement->RotationRate.Yaw *= -1;
+	}
+}
+
---
+++ Source/Playground/Interactables/P_SecurityCamera.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "P_SecurityCamera.generated.h"
+
+UCLASS()
+class PLAYGROUND_API AP_SecurityCamera : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AP_SecurityCamera();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Camera Rotation")
+	UStaticMeshComponent* CameraMesh{};
+	
+	UPROPERTY()
+	class URotatingMovementComponent* CameraMovement{};
+	
+	UPROPERTY(EditAnywhere, Category = "Camera Rotation")
+	float Degree{70};
+	UPROPERTY(EditAnywhere, Category = "Camera Rotation")
+	float YawDirection{100};
+	
+	float MaximumRotationToLeft{};
+	float MaximumRotationToRight{};
+	
+	void Investigating();
+	
+};
